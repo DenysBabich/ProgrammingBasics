91ff6b7bc815c81b5b0a3496ffb466c7b91c4dc6
DenysBabich <89969565+DenysBabich@users.noreply.github.com>
2022-06-05 23:22:06 +0300

    Add files via upload

---
+++ 2_6/Lab_6/Lab_6/Program.cs
@@ -0,0 +1,40 @@
+﻿System.Console.InputEncoding = System.Text.Encoding.Unicode;
+System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+int size = Input("Введіть кількість елементів: ");
+
+int[] arr = InputArray(size);
+
+Tree t = new Tree();
+
+t.root = t.CreateTree(arr, 0);
+
+Console.WriteLine();
+Tree.Print(t.root);
+Console.WriteLine();
+
+Console.WriteLine($"Середнє арифметичне: {t.CountAverage()}.");
+
+
+int Input(string text)
+{
+    while(true)
+    {
+        Console.Write(text);
+
+        if (int.TryParse(Console.ReadLine(), out int result))
+            return result;
+        else
+            Console.WriteLine("Введіть коректні дані!"); 
+    }
+}
+
+int[] InputArray(int size)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+        result[i] = Input($"Введіть {i + 1}-й елемент: ");
+
+    return result;
+}
